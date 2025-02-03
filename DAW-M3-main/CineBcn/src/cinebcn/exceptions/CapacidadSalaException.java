34bfaa6d6c70d17f37cbd9dac688a2117395a038
gabrielolmgil <99087669+gabrielolmgil@users.noreply.github.com>
2025-02-03 20:05:27 +0100

    Add files via upload

---
+++ DAW-M3-main/CineBcn/src/cinebcn/exceptions/CapacidadSalaException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cinebcn.exceptions;
+
+/**
+ *
+ * @author gabrielolmgil
+ */
+    public class CapacidadSalaException extends Exception{
+        public CapacidadSalaException (String errorMess) {
+        super(errorMess);
+        }
+    }
---
+++ DAW-M3-main/CineBcn/src/cinebcn/exceptions/TiposSalaExcpetion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cinebcn.exceptions;
+
+/**
+ *
+ * @author gabrielolmgil
+ */
+public class TiposSalaExcpetion extends Exception{
+    public TiposSalaExcpetion (String errorMess) {
+    super(errorMess);
+    }
+}
